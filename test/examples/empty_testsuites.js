098275ff8759ed7cf36f6f7967bfb1667066aaf3
Andy Palmer <andy@andypalmer.com>
2019-09-14 14:42:20 +1000

    We can convert an empty TestSuites from JS back to XML

--- test/examples/empty_testsuites.js
+++ test/examples/empty_testsuites.js
@@ -4,12 +4,16 @@ const TestSuites = require("../../lib/junitxml2js").TestSuites;
 const xml = `
 <?xml version="1.0" encoding="UTF-8"?>
 <testsuites />
-`;
+`.trim();
 
 const js = new TestSuites();
 
-describe("XML to JS", () => {
-  it("should convert XML to an identical JS representation", () => {
+describe("An empty testsuite", () => {
+  it("should convert from XML to JS", () => {
     expect(TestSuites.from(xml)).to.deep.equal(js);
+  });
+
+  it("should convert from JS to XML", () => {
+    expect(js.to_xml()).to.deep.equal(xml);
   });
 });
